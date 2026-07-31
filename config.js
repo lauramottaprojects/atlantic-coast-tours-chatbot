ab44964bd02c869123986767d60a59d69a16893d
lauramottaprojects <96792976+lauramottaprojects@users.noreply.github.com>
2026-07-31 18:00:00 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ATLANTIC_CONFIG = {
+  apiBase: "https://atlantic-coast-tours-chatbot.vercel.app",
+  sheetUrl:
+    "https://docs.google.com/spreadsheets/d/1balBGf8QhZ5dc-RCCAPt2kcrcf6m_YRh0HL_r8bBtJw/export?format=csv&gid=120683740"
+};
